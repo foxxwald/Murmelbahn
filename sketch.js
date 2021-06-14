d77dbc7bd90638bda8a62a2408ad10de4dfc62d2
foxxwald <fo.xxwald@yahoo.de>
2021-06-14 10:11:03 +0200

    ?

--- sketch.js
+++ sketch.js
@@ -14,31 +14,48 @@ let circle;
 let obstacle;
 let slide;
 
-
 const drawBody = Helpers.drawBody;
 const drawSprite = Helpers.drawSprite;
 
 let hitsound;
-let soundbox;
+let soundfeld;
 let ground;
 let ball;
 let canvas;
 let ballImg;
+let soundfeldImg;
+
+let treppe1;
+let treppe2;
+let treppe3;
+let treppe4;
+
+function preload() {
+  httpGet("svg/soundfeld.svg", "text", false, function (response) {
+    // when the HTTP request completes ...
+    // 1. parse the svg and get the path
+    const parser = new DOMParser();
+    const svgDoc = parser.parseFromString(response, "image/svg+xml");
+    const svgPathElement = svgDoc.querySelector("path");
+    // 2. setup all matter.js related things
+    setupMatter(svgPathElement);
+  });
+}
 
 
 
-function setup() {
+function setupMatter(svgPathElement) {
 
-  let canvas = createCanvas(windowWidth, windowHeight)
+  let canvas = createCanvas(1200, windowHeight * 2)
   canvas.parent('theCanvas')
+
+  ballImg = loadImage('Bilder/ball.png');
 
   engine = Engine.create();
 
-  ballImg = loadImage('Bilder/ball.svg');
 
-  ball = Bodies.circle(100, 50, 40, {
-    restitution: 0
-  });
+
+  ball = Bodies.circle(100, 50, 10, { restitution: 0 });
 
   ball.plugin.wrap = {
     min: { x: 0, y: 0 },
@@ -47,28 +64,52 @@ function setup() {
 
 
  /*  schiefe Ebene schwarz  */ground = Bodies.rectangle(148, 120, 230, 10, {
-    isStatic: true, angle: Math.PI * 0.01
+    isStatic: true, angle: Math.PI * 0.03
   });
 
-   /*  soundkasten  */ soundbox = Bodies.rectangle(340, 120, 110, 50, {
+  soundfeld = Bodies.fromVertices(340, 180, Matter.Svg.pathToVertices(svgPathElement), {
+    isStatic: true, scale: 0, label: 'soundfeld'
+  });
+
+
+
+  rectMode(CORNER);
+  //TREPPE
+  /*  Treppe1  */ treppe1 = Bodies.rectangle(445, 325, 95, 6, {
     isStatic: true,
   });
 
+
+  /*  Treppe2  */ treppe2 = Bodies.rectangle(463, 341, 125, 6, {
+    isStatic: true,
+  });
+
+    /*  Treppe3  */ treppe3 = Bodies.rectangle(485, 355, 170, 6, {
+    isStatic: true,
+  });
+
+    /*  Treppe4  */ treppe4 = Bodies.rectangle(515, 370, 230, 6, {
+    isStatic: true,
+  });
+
+
+
   //load sound
-  hitSound = loadSound("sound/flutinstrumental.mp3");
-  hitSound.playMode('sustain');
+  hitsound = loadSound("Sound/kla4mix.mp3");
+  hitsound.playMode('sustain');
 
   // setup hit sound
   Matter.Events.on(engine, 'collisionStart', function (event) {
     const pairs = event.pairs[0];
     const bodyA = pairs.bodyA;
     const bodyB = pairs.bodyB;
-    if (bodyA.label === "soundbox" || bodyB.label === "soundbox") {
-      hitSound.play();
-    }
+    if (bodyA.label === "soundfeld" || bodyB.label === "soundfeld") {
+      hitsound.play();
+
+    } console.log(bodyA.label)
   });
 
-  World.add(engine.world, [ball, ground, soundbox]);
+  World.add(engine.world, [ball, ground, soundfeld, treppe1, treppe2, treppe3, treppe4, hitsound]);
 
   Engine.run(engine);
 }
@@ -76,22 +117,34 @@ function setup() {
 function draw() {
   clear();
 
+  scale(2);
+
   drawSprite(ball, ballImg);
+
 
   noStroke();
   fill(0);
   drawBody(ground);
 
+  scrollFollow(ball);
+
 
   // visualize collision
-  const collided = Matter.SAT.collides(soundbox, ball).collided;
+  const collided = Matter.SAT.collides(soundfeld, ball).collided;
   if (collided) {
     fill('red');
   } else {
-    fill('white');
+    fill('pink');
   }
 
-  drawBody(soundbox);
+  rectMode(CORNER);
+  drawBody(treppe1);
+  drawBody(treppe2);
+  drawBody(treppe3);
+  drawBody(treppe4);
+  /* drawBody(soundfeld); */
+
+
 
 
   /* fill(0);
@@ -104,13 +157,38 @@ function keyPressed() {
   if (keyCode === 32) {
     let direction = 1; // circle runs left to right ->
     if ((ball.position.x - ball.positionPrev.x) < 0) {
-      direction = -1; // circle runs right to left <-
+      direction = 1;
+      //direction = -1; circle runs right to left <-
     }
     // use current direction and velocity for the jump
     Body.applyForce(
       ball,
       { x: ball.position.x, y: ball.position.y },
-      { x: (0.01 * direction) + ball.velocity.x / 100, y: -0.1 }
+      { x: (0.001 * direction), y: -0.005 }
     );
+  }
+}
+
+//Scrollfollow
+function scrollFollow(matterObj) {
+  const $element = $('#parent');
+  if (insideViewport($element, matterObj) == false) {
+    if ($element.is(':animated') == false) {
+      $element.animate({
+        /*     scrollLeft: ball.position.x, */
+        scrollTop: (Math.floor((ball.position.y / 675)) * 675)
+      }, 100);
+    }
+  }
+}
+
+function insideViewport(element, matterObj) {
+  const x = matterObj.position.x;
+  const y = matterObj.position.y;
+  const pageYOffset = 675;
+  if (y <= pageYOffset) {
+    return true;
+  } else {
+    return false;
   }
 }
